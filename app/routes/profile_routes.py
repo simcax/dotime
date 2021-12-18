4a4b80542e312b8b000b1e3a9ac02ce305e84619
Carsten Skov <carsten@simcax.dk>
2021-12-18 07:13:49 +0100

    Profile creation endpoint now inserts user in database

--- app/routes/profile_routes.py
+++ app/routes/profile_routes.py
@@ -1,6 +1,7 @@
 '''Routes for profile creation'''
 
 from flask import Blueprint, render_template, request
+from app.profile.profile import ProfileHandling
 
 bp = Blueprint('profile_blueprint', __name__, url_prefix='/profile')
 
@@ -8,7 +9,15 @@ bp = Blueprint('profile_blueprint', __name__, url_prefix='/profile')
 def create_profile():
     '''Endpoint for creating a profile'''
     if request.method == 'POST':
-        return_value = "Your profile was created!"
+        username = request.form['profileUsername']
+        email = request.form['profileEmail']
+        password = request.form['profilePassword']
+        prof = ProfileHandling()
+        userid = prof.add_user(username,password,email)
+        if userid:
+            return_value = "Your profile was created!"
+        else:
+            return_value = "Profile creation failed."
     else:
         return_value = render_template('create_profile.html')
     return return_value
