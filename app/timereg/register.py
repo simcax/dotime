f2a2dc0deea0cf4456f518cd669797c13842b099
Carsten Skov <carsten@simcax.dk>
2021-12-31 07:51:34 +0100

    Added checks for timereg timestamps
    Time from needs to be before time to, 
    Timeto and timefrom needs to be timestamps

--- app/timereg/register.py
+++ app/timereg/register.py
@@ -1,4 +1,5 @@
 '''Module handling time registration'''
+from datetime import datetime
 from psycopg2 import DatabaseError
 from app.db import database
 
@@ -30,28 +31,31 @@ class TimeRegistration:
     def add_timeregistration(self,activity_uuid, timefrom, timeto):
         '''Adds a row to the time registration table with a link record to an activity uuid'''
         timereg_added = False
-        try:
-            db = database.Database()
-            conn = db.connect()
-            with conn.cursor() as cur:
-                
-                
-                sql = f"INSERT INTO soc.timedmeetgo (timefrom, timeto, usersId) \
-                    VALUES ('{timefrom}','{timeto}','{self.userid}') RETURNING timedmeetgouuid"
-                cur.execute(sql)
-                timed_meet_go_uuid = cur.fetchone()[0]
+        if isinstance(timefrom,datetime) and isinstance(timeto,datetime) and timefrom < timeto:
+            try:
+                db = database.Database()
+                conn = db.connect()
+                with conn.cursor() as cur:
+                    
+                    
+                    sql = f"INSERT INTO soc.timedmeetgo (timefrom, timeto, usersId) \
+                        VALUES ('{timefrom}','{timeto}','{self.userid}') RETURNING timedmeetgouuid"
+                    cur.execute(sql)
+                    timed_meet_go_uuid = cur.fetchone()[0]
 
-                sql = f"INSERT INTO soc.ln_timemeetgo (timedmeetgouuid,activitesuuid) \
-                    VALUES ('{timed_meet_go_uuid}','{activity_uuid}')"
-                cur.execute(sql)
-                if cur.rowcount == 1:
-                    conn.commit()
-                    timereg_added = True
-                else:
-                    timereg_added = False
-        except DatabaseError as error:
-            print(f"Error with sql {sql} - {error}")
-        finally:
-            conn.close()
+                    sql = f"INSERT INTO soc.ln_timemeetgo (timedmeetgouuid,activitesuuid) \
+                        VALUES ('{timed_meet_go_uuid}','{activity_uuid}')"
+                    cur.execute(sql)
+                    if cur.rowcount == 1:
+                        conn.commit()
+                        timereg_added = True
+                    else:
+                        timereg_added = False
+            except DatabaseError as error:
+                print(f"Error with sql {sql} - {error}")
+            finally:
+                conn.close()
+        else:
+            timereg_added = False
         return timereg_added
 
--- test_timereg.py
+++ test_timereg.py
@@ -35,3 +35,27 @@ def test_add_timereg_row(create_user):
     assert isinstance(activity_uuid, str)
     timereg_added = time_reg.add_timeregistration(activity_uuid, timefrom, timeto)
     assert timereg_added == True
+
+def test_add_timereg_wrong_order(create_user):
+    '''Test getting an error when adding a time reg record with to before from timestamp'''
+    time_reg = TimeRegistration(create_user['users_id'])
+    tu = TestUtils()
+    activity_name_str = tu.createRandomString()
+    activity_uuid = time_reg.add_activity(activity_name_str)
+    timeto = datetime.datetime.now()
+    timefrom = timeto + datetime.timedelta(minutes=2)
+    assert isinstance(activity_uuid, str)
+    timereg_added = time_reg.add_timeregistration(activity_uuid, timefrom, timeto)
+    assert timereg_added == False
+
+def test_add_timereg_correct_timestamps(create_user):
+    '''Test a timestamp check is carried out when submitting a timereg entry'''
+    time_reg = TimeRegistration(create_user['users_id'])
+    tu = TestUtils()
+    activity_name_str = tu.createRandomString()
+    activity_uuid = time_reg.add_activity(activity_name_str)
+    timefrom = datetime.datetime.now()
+    timeto = str(timefrom + datetime.timedelta(minutes=2)) + "1a"
+    assert isinstance(activity_uuid, str)
+    timereg_added = time_reg.add_timeregistration(activity_uuid, timefrom, timeto)
+    assert timereg_added == False
