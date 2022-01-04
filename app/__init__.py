8e5304acc63072228cf8cd5398099af0062e36a3
Carsten Skov <carsten@simcax.dk>
2022-01-04 07:36:24 +0100

    Added session handling and cookie creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,29 @@
 '''Do Time Flask App'''
 from os import environ, urandom
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, session
+from flask_session import Session
+import redis
 
 def create_app(test_config=None):
     '''App factory'''
+    REDIS_HOST = environ.get('REDIS_HOST','localhost')
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY=environ.get('SECRET_KEY',urandom(12).hex())
+        SECRET_KEY=environ.get('SECRET_KEY',urandom(12).hex()),
+        SESSION_TYPE = 'redis',
+        SESSION_REDIS = redis.from_url(f"redis://{REDIS_HOST}:6379"),
+        SESSION_PERMANENT = True,
+        SESSION_USE_SIGNER = True,
+        SESSION_COOKIE_SECURE = True,
+        SESSION_COOKIE_DOMAIN = environ.get('SESSION_COOKIE_DOMAIN','127.0.0.1'),
+        SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME','DoTime')
     )
     if test_config:
         app.logger.info("Test config is set")
-
+    app.logger.info(app.config)
+    sess = Session()
     with app.app_context():
+        sess.init_app(app)
         from app.routes import (
             profile_routes, health_routes, auth_routes, image_routes, session_routes
         )
--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [ `docker ps -a -f name=^redis|grep redis|wc -l` -eq 1 ];then 
+    echo "Redis container exists"
+    docker start redis
+else
+    echo "Starting redis"
+    docker run -d -p 6379:6379 redis --name redis
+fi
 pushd $HOME/projekter/dotime/docker/cockroachdb
 if [ `docker-compose top|grep roach|wc -l` -eq 6 ]; then
     echo "Cockroachdb is already running"
@@ -17,6 +24,9 @@ export DB_SSL_MODE=disable
 export DB_SSL=false
 export DB_ROOT_CERT_PATH=/tmp/.postgresql/root.crt
 export SECRET_KEY=d2563cb665a7b04fac77ccf4
+export SESSION_REDIS=redis://localhost:6379
+export SESSION_TYPE=redis
+export REDIS_HOST=localhost
 export FLASK_APP=app
 export FLASK_ENV=DEVELOPMENT
 export FLASK_DEBUG=TRUE
