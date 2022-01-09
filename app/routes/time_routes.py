1d836c963abfcfe0bc7f6e24a4e8f6a483ecaef7
Carsten Skov <carsten@simcax.dk>
2022-01-09 07:23:22 +0100

    Refactoring method name for time entering

--- app/routes/time_routes.py
+++ app/routes/time_routes.py
@@ -6,7 +6,7 @@ from app.utils import date_utils
 bp = Blueprint('time_blueprint', __name__, url_prefix='/time')
 
 @bp.route("/enter", methods=["GET", "POST"])
-def register_time():
+def enter_time():
     '''Endpoint for time registration'''
     year, weeknumber, daynumber = datetime.isocalendar(datetime.now())
     date_info = { 'year': year, 'weeknumber': weeknumber, 'daynumber': daynumber}
