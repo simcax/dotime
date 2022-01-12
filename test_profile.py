0a9668d539e2726d13a59dbea4da7dcf8ef3bf2e
Carsten Skov <carsten@simcax.dk>
2022-01-12 06:03:09 +0100

    Refactored to use generic test utils method

--- test_profile.py
+++ test_profile.py
@@ -7,22 +7,19 @@ from werkzeug.security import check_password_hash
 from app.profile.profile import ProfileHandling
 from test_utils import TestUtils
 
-def createRandomString():
-    ''''Helper function to create a random string - 10 chars long'''
-    randomString = ''.join(random.choice(string.ascii_letters) for x in range(10))
-    return randomString
-
 def test_generate_password():
     '''Test a password can be created, and then validated by werkzeug check_password_hash function'''
     prof = ProfileHandling()
-    password = createRandomString()
+    tu = TestUtils()
+    password = tu.createRandomString()
     hashed_password = prof.create_password(password)
     assert check_password_hash(hashed_password,password)
 
 def test_password_check():
     '''Checking to see a generated password can be validated'''
     prof = ProfileHandling()
-    password = createRandomString()
+    tu = TestUtils()
+    password = tu.createRandomString()
     hashed_password = prof.create_password(password)
     assert prof.validate_password(hashed_password,password)
 
