95998c8c14c253b82a21fec245931ffd6b0959bc
Carsten Skov <carsten@simcax.dk>
2022-01-23 12:27:58 +0100

    Fixed missing app context for test

--- test_settings.py
+++ test_settings.py
@@ -47,18 +47,19 @@ def test_set_defaults_on_no_settings(create_user):
     user_settings_2 = settings.get_settings(user_id)
     assert len(user_settings_2) == 14
     
-def test_get_workday_defaults(create_user):
+def test_get_workday_defaults(create_user, app_test_context):
     '''Test retrieving settings about the defaults for a work week'''
-    settings = SettingsHandling()
-    userdata = create_user['user_id']
-    user_id = userdata['users_id']
-    # Make sure we have added default workday lengths to the users profile
-    settings.add_defaults(user_id)
-    user_settings = settings.get_settings(user_id)
-    # Retrieve an object with the lenght of all the workdays.
-    # This should be for Monday through Sunday.
-    # The method should add any missing days with a 0 hours and 0 minutes
-    workweek_day_lengths = settings.get_workweek_day_lengths(user_id)
-    for i in range(1,8):
-        assert workweek_day_lengths.get(f"workdayLength{i}Hour", 'empty') != 'empty'
-        assert workweek_day_lengths.get(f"workdayLength{i}Minutes", 'empty') != 'empty'
+    with app_test_context:
+        settings = SettingsHandling()
+        userdata = create_user['user_id']
+        user_id = userdata['users_id']
+        # Make sure we have added default workday lengths to the users profile
+        settings.add_defaults(user_id)
+        user_settings = settings.get_settings(user_id)
+        # Retrieve an object with the lenght of all the workdays.
+        # This should be for Monday through Sunday.
+        # The method should add any missing days with a 0 hours and 0 minutes
+        workweek_day_lengths = settings.get_workweek_day_lengths(user_id)
+        for i in range(1,8):
+            assert workweek_day_lengths.get(f"workdayLength{i}Hour", 'empty') != 'empty'
+            assert workweek_day_lengths.get(f"workdayLength{i}Minutes", 'empty') != 'empty'
