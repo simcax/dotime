3f2bca4f43c26e28125a94cd401abd9b5a27afdc
Carsten Skov <carsten@simcax.dk>
2022-08-28 17:31:49 +0200

    Added condition on drop of constraint

--- database/sql/V3__ALTER_LN_TIMEMEETGO.sql
+++ database/sql/V3__ALTER_LN_TIMEMEETGO.sql
@@ -1,4 +1,4 @@
-ALTER TABLE soc.ln_timemeetgo DROP CONSTRAINT fk_activitesuuid_ref_users;
-ALTER TABLE soc.ln_timemeetgo DROP CONSTRAINT fk_timedmeetgouuid_ref_users;
+ALTER TABLE soc.ln_timemeetgo DROP CONSTRAINT IF EXISTS fk_activitesuuid_ref_users;
+ALTER TABLE soc.ln_timemeetgo DROP CONSTRAINT IF EXISTS fk_timedmeetgouuid_ref_users;
 ALTER TABLE soc.ln_timemeetgo ADD CONSTRAINT fk_activitesuuid_ref_users FOREIGN KEY (timedmeetgouuid) REFERENCES soc.timedmeetgo (timedmeetgouuid) ON DELETE CASCADE;
 ALTER TABLE soc.ln_timemeetgo ADD CONSTRAINT fk_timedmeetgouuid_ref_users FOREIGN KEY (activitesuuid) REFERENCES soc.activites (activitesuuid) ON DELETE CASCADE;
